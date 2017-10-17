f8b61bdf48ac8af5f74eed38bd04cd834a04c878
jamescross91 <james_cross91@hotmail.com>
2017-10-17 14:40:31 +0100

    Adding lambda handler

---
+++ src/main/java/Lambda.java
@@ -0,0 +1,26 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import config.Config;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Lambda implements RequestStreamHandler, RequestHandler<Object, Object>{
+    @Override
+    public Object handleRequest(Object o, Context context) {
+        return null;
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        String JSONConfig = IOUtils.toString(inputStream);
+        System.out.println("JSON input is: " + JSONConfig);
+        Config config = new Config(JSONConfig);
+
+        AdFeedProcessor adFeedProcessor = new AdFeedProcessor(config);
+        adFeedProcessor.process();
+    }
+}
