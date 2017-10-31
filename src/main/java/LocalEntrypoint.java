3599457a08eefde3063ba9254854329410b1177a
jamescross91 <james_cross91@hotmail.com>
2017-10-31 18:22:01 +0000

    Adding combiner

--- src/main/java/LocalEntrypoint.java
+++ src/main/java/LocalEntrypoint.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class LocalEntrypoint {
     public static void main(String args[]) throws IOException {
-        Config config = Config.fromFile("/Users/James/Developer/dynamic-ad-feed-converter/src/main/resources/amc-google-config.json");
+        Config config = Config.fromFile("/Users/James/Developer/dynamic-ad-feed-converter/src/main/resources/sample-config.json");
 
         AdFeedProcessor adFeedProcessor = new AdFeedProcessor(config);
         adFeedProcessor.process();
--- src/main/java/output/map/Mapper.java
+++ src/main/java/output/map/Mapper.java
@@ -28,13 +28,31 @@ public abstract class Mapper {
         return data;
     }
 
-    String getValue(String field) {
+    private String getValue(String field) {
         String value = sourceRecord.get(field);
 
         if(field.contains("%STATIC:")) {
-            return  field.replace("%STATIC:", "");
+            return field.replace("%STATIC:", "");
+        }
+
+        if (field.contains("%COMBINER:")) {
+            return getValueCombine(field);
         }
 
         return value;
+    }
+
+    private String getValueCombine(String mappingRule) {
+        String[] fields = mappingRule.replace("%COMBINER:", "").replace(" ", "").split(",");
+
+        StringBuilder builder = new StringBuilder();
+        for (String field : fields) {
+            if (sourceRecord.containsKey(field)) {
+                builder.append(sourceRecord.get(field));
+                builder.append(" ");
+            }
+        }
+
+        return builder.toString();
     }
 }
