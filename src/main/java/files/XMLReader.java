d409321a2638f3e416d09d9f8a5dec9f2102d814
jamescross91 <james_cross91@hotmail.com>
2017-11-19 20:30:07 +0000

    Accelerate XML parser

--- src/main/java/files/XMLReader.java
+++ src/main/java/files/XMLReader.java
@@ -15,11 +15,10 @@ import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class XMLReader implements Reader {
     private final Config config;
@@ -32,48 +31,31 @@ public class XMLReader implements Reader {
     public List<Map<String, String>> read(String filePath) throws IOException, ParserConfigurationException, SAXException, XPathExpressionException {
         Document xmlDocument = getDocument(filePath);
 
+        List<Map<String, String>> dataList = new ArrayList<>();
 
-        List<Map<String, String>> rootElements = processExpression(xmlDocument, config.getXPathRoot())
-            .stream()
-            .map(x -> {
-                HashMap<String, String> map = new HashMap<>();
-                map.putAll(config.getXPathQueries());
-                return map;
-            })
-            .collect(Collectors.toList());
+        XPath xPath = XPathFactory.newInstance().newXPath();
 
-        for (int i = 1; i <= rootElements.size(); i++) {
-            System.out.println("Querying element " + i + " of " + rootElements.size());
-            rootElements.set(i - 1, getDataForItem(xmlDocument, rootElements.get(i - 1), i));
+        NodeList nodes = (NodeList) xPath.evaluate(config.getXPathRoot(),
+            xmlDocument.getDocumentElement(), XPathConstants.NODESET);
+
+        for (int i = 0; i < nodes.getLength(); i++) {
+            evaluateNodeExpressions(dataList, xPath, nodes, i);
         }
 
-        return rootElements;
+        return dataList;
     }
 
-    private Map<String, String> getDataForItem(Document xmlDocument, Map<String, String> item, int index) throws XPathExpressionException {
+    private void evaluateNodeExpressions(List<Map<String, String>> dataList, XPath xPath, NodeList nodes, int i) throws XPathExpressionException {
+        Node singleNode = nodes.item(i);
+        singleNode.getParentNode().removeChild(singleNode);
+
         Map<String, String> dataMap = new HashMap<>();
-        for (Map.Entry<String, String> entry : item.entrySet()) {
-            getData(xmlDocument, index, dataMap, entry);
+
+        for (Map.Entry<String, String> entry : config.getXPathQueries().entrySet()) {
+            dataMap.put(entry.getKey(), xPath.evaluate(entry.getValue(), singleNode));
         }
 
-        return dataMap;
-    }
-
-    private void getData(Document xmlDocument, int index, Map<String, String> dataMap, Map.Entry<String, String> entry) throws XPathExpressionException {
-        String query = buildQuery(index, config.getXPathRoot(), entry.getValue());
-        List<String> data = processExpression(xmlDocument, query);
-
-        if (data.size() > 1) throw new IllegalStateException("Found more than one result for query " + query);
-
-        if (data.size() == 0) {
-            dataMap.put(entry.getKey(), "");
-        } else {
-            dataMap.put(entry.getKey(), data.get(0));
-        }
-    }
-
-    private String buildQuery(int index, String queryRoot, String queryExpression) {
-        return queryRoot + "[" + Integer.toString(index) + "]" + queryExpression;
+        dataList.add(dataMap);
     }
 
     private Document getDocument(String filePath) throws IOException, ParserConfigurationException, SAXException {
@@ -81,15 +63,5 @@ public class XMLReader implements Reader {
         DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = builderFactory.newDocumentBuilder();
         return builder.parse(fileIS);
-    }
-
-    private List<String> processExpression(Document xmlDocument, String expression) throws XPathExpressionException {
-        XPath xPath = XPathFactory.newInstance().newXPath();
-        NodeList nl = (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
-
-        return IntStream.range(0, nl.getLength())
-            .mapToObj(nl::item)
-            .map(Node::getTextContent)
-            .collect(Collectors.toList());
     }
 }
